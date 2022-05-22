4fbfe6a41a275a382cffc5bb1d133ee5dcd7a153
pogial <pogial@hotmail.com>
2022-05-22 13:30:48 -0500

    Encabezado

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens:{
+        'xs': {'max':'768px'},
+      },
       backgroundImage: {
         'body-pattern': "url('../img/pattern.png')",
-      }
+      },
+      colors: {
+        'light-blue': '#37bcf9',
+        'dark-blue': '#0370b9'
+      },
+      animation: {
+        'spin-slow': 'spin 2s linear infinite',
+      },
     },
   },
   plugins: [],
